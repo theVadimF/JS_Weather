d63f84c50dd1e6f84127383a38bd7ae608d04c56
Vadim Faust <vadim40328@gmail.com>
2023-03-02 18:49:15 +0700

    Left panel layout

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import './styles.css'
 import 'normalize.css'
 import 'line-awesome/dist/line-awesome/css/line-awesome.css'
-console.log("WTF")
--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,7 @@ module.exports = {
       }, {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      }, 
-      // {
-      //   test: /\.html$/,
-      //   loader: "raw-loader"
-      // },
+      },
     ],
   },
   resolve: {
